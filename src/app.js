f4ad6170671d0c7bdbd24c8d8599b0d2805d51fd
filipesaretta <filipessm124@gmail.com>
2020-06-17 18:09:45 +0200

    Add func to check if id is valid

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -9,6 +9,18 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+
+function validateId(request, response, next) {
+  const { id } = request.params;
+  if (!isUuid(id)) {
+    return response
+      .status(400)
+      .json({ error: 'The ID is not valid, please try again' });
+  }
+  return next();
+}
+
+app.use('/repositories/:id', validateId);
 
 app.get('/repositories', (request, response) => response.json(repositories));
 
